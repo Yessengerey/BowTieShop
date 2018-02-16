7673f077f2148d5efbd02f5fb363f78b6166bd44
Gary <yes.murz@gmail.com>
2018-02-16 17:41:13 +0600

    Initial set up for routing

---
+++ client/src/containers/about.jsx
@@ -0,0 +1,16 @@
+// React
+import React, { Component } from 'react';
+
+export default class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        About
+      </div>
+    )
+  }
+}
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,6 +5,12 @@ import ReactDOM from 'react-dom';
 // Material UI
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+// React Router
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+
+// Custom Components
+import About from './containers/about.jsx';
+
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -13,10 +19,19 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <nav>
+          <Link to="/about">О нас</Link>
+        </nav>
+        <div>
+          <Route path="/about" component={About}/>
+        </div>
       </div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('app'));
