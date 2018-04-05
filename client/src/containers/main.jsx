4eb4a947a7184eadf4ef34d5af19acde51b30f04
Gary <yes.murz@gmail.com>
2018-04-05 14:08:31 +0600

    add footer

--- client/src/containers/main.jsx
+++ client/src/containers/main.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import Header from '../components/header.jsx';
+import Footer from '../components/footer.jsx';
+
+import styles from '../../../styles/main.css';
 
 export default class Main extends Component {
   constructor(props) {
@@ -12,8 +15,14 @@ export default class Main extends Component {
 
   render() {
     return (
-      <div style={{height: '100%'}}>
+      <div className={styles.main_outer_container}>
         <Header />
+        {/* <div className={styles.main_inner_container}>
+          <section className={styles.parallax_section}>
+            <h1>Such Adorableness</h1>
+          </section>
+        </div> */}
+        <Footer />
       </div>
     )
   }
